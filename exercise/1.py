c738e5f78f7a0e6b705d20564190f2e2a8de8075
zhanglu_502 <zhangludada@hotmail.com>
2020-08-18 09:28:55 +0800

    2020-08-18(1)

---
+++ exercise/1.py
@@ -0,0 +1,18 @@
+def same_word(s1,s2):
+    count1=[0 for x in range(26)]
+    count2=[0 for x in range(26)]
+    for i in s1:
+        id=ord(i)-ord('a')
+        count1[id]+=1
+    for i in s2:
+        id=ord(i)-ord('a')
+        count2[id]+=1
+    if count1==count2:
+        return True
+    else:
+        return False
+
+a='eabcd'
+b='cdbae'
+print(same_word(a,b))
+
